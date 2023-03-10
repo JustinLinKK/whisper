3e97d0656c5284bf9eb8a07547ec50bcff97db79
JustinLinKK <lin88@mcmaster.ca>
2023-03-09 19:38:09 -0500

    Add integration to the discord bot

---
+++ DiscordBot.py
@@ -0,0 +1,67 @@
+import discord
+from discord.ext import commands
+import whisper
+
+
+
+TOKEN = 'MTA4MzEwNzE4ODc4MTg4NzUxOA.GRNnv0.lhbQ10Qp1QRj8YBKjoG5B25M2PsKJQ4NshFMPY'
+intents = discord.Intents.default()
+intents.message_content = True
+
+model = whisper.load_model("base")
+
+
+# 2
+bot = commands.Bot(command_prefix='$',intents=intents)
+
+@bot.command()
+async def test(ctx,arg):
+    await ctx.send(arg)
+
+
+@bot.command()
+async def audioToText(ctx):
+    files = []
+    for attachment in ctx.message.attachments:
+        # fp = BytesIO()
+        audioName = attachment.filename
+        await attachment.save(attachment.filename)
+        result = model.transcribe(audioName)
+        await ctx.send(result["text"])
+        
+
+
+    
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+        
+
+    
+        
+		
+                
+    
+    
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+
+bot.run(TOKEN)
+
+
